b8be3d00a79978f895b8be82078a667a2eac4970
tskorohod <tskorohod@lmteam.by>
2022-04-27 09:20:49 +0300

    started doing test task

--- test_task/src/App.js
+++ test_task/src/App.js
@@ -1,8 +1,10 @@
-import './App.css';
+import './App.scss';
+import DropDownList from './drop-down-list/DropDownList';
 
 function App() {
 	return (
 		<div className="App">
+			<DropDownList></DropDownList>
 		</div>
 	);
 }
---
+++ test_task/src/drop-down-list/DropDownList.jsx
@@ -0,0 +1,13 @@
+import './_drop-down-list.scss';
+
+const DropDownList = () => {
+	return (
+		<select>
+			<option value="1"></option>
+			<option value="2"></option>
+			<option value="3"></option>
+		</select>
+	)
+}
+
+export default DropDownList;
