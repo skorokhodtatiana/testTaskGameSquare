4c315bb09d174cfeba93186fc2d5c16df630c548
tskorohod <tskorohod@lmteam.by>
2022-05-03 22:02:20 +0300

    in process of drawing a square

--- test_task/src/table/Table.jsx
+++ test_task/src/table/Table.jsx
@@ -3,9 +3,11 @@ import './_table.scss';
 const Table = (props) => {
 	const {option} = props;
 	console.log(option);
+	
 	for(let i = 0; i <= option; i++) {
 			<div className='cell'>cell</div>
 	}
+	
 }
 
 export default Table;
