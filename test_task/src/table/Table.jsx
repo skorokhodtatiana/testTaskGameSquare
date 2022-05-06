f7fd0f51eb357fbc2711a025100d4dc35a05bed4
tskorohod <tskorohod@lmteam.by>
2022-05-06 16:19:37 +0300

    got array from number

--- test_task/src/table/Table.jsx
+++ test_task/src/table/Table.jsx
@@ -3,9 +3,25 @@ import './_table.scss';
 const Table = (props) => {
 	const {option} = props;
 	console.log(option);
-	const rows1 = (() => new Array());
-	const rows = (() => new Array().fill(option),[option]);
-	console.log(rows);
+	const arr = [];
+	let numSquare = Number(option);
+	for (numSquare; numSquare > 0; numSquare--) {
+		arr.push(option);
+	}
+	return(
+		<div>
+			{arr.map((item, indexItem) => {
+		return (
+			<div key={`${item}${indexItem}`}>one</div>
+		)
+	})}
+		</div>
+	)
+	
+
+	// const rows1 = (() => new Array());
+	// const rows = (() => new Array().fill(option),[option]);
+	// console.log(arr);
 		
 }
 
