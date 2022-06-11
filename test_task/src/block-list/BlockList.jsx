b3daa2ec9a2ad4f1c938c1d0c02dd9d5a5dfba8a
tskorohod <tskorohod@lmteam.by>
2022-06-11 22:51:51 +0300

    d

--- test_task/src/block-list/BlockList.jsx
+++ test_task/src/block-list/BlockList.jsx
@@ -1,9 +1,5 @@
 const ShowListBlue = ({hoveredItems}) => {
 
-	// const [isListBlueSquare, setisListBlueSquare] = useState('');
-	// const [isArr, setIsArr] = useState([]);
-	// const [arr, setArr] = useState([]);
-
 	return (
 		<div>
 			<div id="list" className="listBlueSquare"></div>
